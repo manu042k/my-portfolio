9154cedc98b03fe08fd796e31a8555e9549b2987
Manoj <manu042k@gmail.com>
2026-03-14 12:38:29 -0700

    refactor: remove old UI components for redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavBarComponent } from './navbar/nav-bar.component';
-import { HomeComponent } from './home/home.component';
-import { SkillsSectionComponent } from './skills-section/skills-section.component';
-import { TimelineComponent } from './timeline/timeline.component';
-import { ProjectSectionComponent } from './project-section/project-section.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-import { AwardsComponent } from './awards/awards.component';
 
 @Component({
-    imports: [
-        RouterModule,
-        NavBarComponent,
-        HomeComponent,
-        SkillsSectionComponent,
-        TimelineComponent,
-        ProjectSectionComponent,
-        FooterComponent,
-        ContactComponent,
-        AwardsComponent,
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
